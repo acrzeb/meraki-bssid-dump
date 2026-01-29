fc8c3056e8eba58dd74fef13129be5cb6766ff67
Zack <109986221+acrzeb@users.noreply.github.com>
2026-01-29 17:01:19 -0500

    General Refactor: Resolves #14 by Refactoring to use getOrganizationDevices, Improves Interactive UX, Updates Readme (#16)

--- README.md
+++ README.md
@@ -2,37 +2,36 @@
 [![Run in Cisco Cloud IDE](https://static.production.devnetcloud.com/codeexchange/assets/images/devnet-runable-icon.svg)](https://developer.cisco.com/devenv/?id=devenv-vscode-base&GITHUB_SOURCE_REPO=https://github.com/shantarsecurity/meraki-bssid-dump)
 # Meraki BSSID Dump
 
-🔍 This project, **'Meraki BSSID Dump'**, is a Python script created to export BSSID (Basic Service Set Identifier) information from an organization's Meraki wireless networks. The primary use case is to retrieve BSSID details for manual import into geolocation data services (e.g. e911). It provides network and facility administrators with a simplified method to gather bulk BSSID information.
+🔍 This project, **'Meraki BSSID Dump'**, is a Python script that exports BSSID (Basic Service Set Identifier) information for an organization's Meraki wireless networks.
 
-CSV file example output:
+## Use Cases
 
-| Network ID | Network Name | Device Model | Device Serial  | Device Base MAC   | LAN IP          | Display Name | Location Address   | Longitude | Latitude | Floor Plan ID | Notes     | BSSID              | SSID      | Band  |
-|------------|--------------|--------------|----------------|-------------------|-----------------|--------------|--------------------|-----------|----------|---------------|-----------|--------------------|-----------|-------|
-| A_12345678 | MyNetwork1   | CW1234L      | A12B-CD34-E56F | a1:b2:c3:d4:e5:f6 | 123.456.789.101 | Device1      | 123 A Street NW    | -45.678   |  90.123  | None          |           | 00:11:22:33:44:55  | Network1  | 2.4GHz|
-| A_12345678 | MyNetwork1   | MR99         | GH78-I9J0-1K2L | 1a:2b:3c:4d:5e:6f | 121.314.151.617 | Device1      | 456 D Aveneue SE   | -90.123   | -45.678  | None          |           | 66:77:88:99:AA:BB  | Network1  | 5GHz  |
-| A_12345678 | MyNetwork1   | MR01         | 3M45-O6N8-9Q09 | ab:12:cd:34:ef:56 | 181.920.212.223 | Device2      | 789 13th Blvd      |  45.678   | -90.123  | None          |           | CC:DD:EE:FF:00:11  | Network2  | 2.4GHz|
-
-The document "[Calculating Cisco Meraki BSSID MAC Addresses](https://documentation.meraki.com/MR/Wi-Fi_Basics_and_Best_Practices/Calculating_Cisco_Meraki_BSSID_MAC_Addresses)" offers a comprehensive guide on how BSSID MAC addresses are computed for Cisco Meraki access points. Each SSID is represented by a unique BSSID, which helps clients identify the associated access point. The BSSID addresses are derived from the wired MAC address, with calculations based on the MAC OUI. Different MAC OUIs for various access point models determine the adjustments for 2.4 GHz and 5 GHz bands. The BSSID values are assigned to each radio and SSID, with up to 15 possible BSSID combinations per radio. 
+🎯 The script's primary use case is to export BSSID details for import into geolocation services (e.g., e911 services).\
+\
+🏢 This is often necessary for large organizations like, Colleges and Universities, Hospitals and Healthcare Facilities, and Private Enterprise Networks.
 
 ## Technology Stack
 
 🔧 The script is written in Python and utilizes the [Meraki Dashboard API Python Library](https://github.com/meraki/dashboard-api-python/tree/main) to interact with the [Meraki Dashboard API](https://developer.cisco.com/meraki/api-v1/).
 
-🐍 Python was chosen for its simplicity and the comprehensive support provided by Meraki's Python SDK.
+🐍 Python was chosen for its simplicity and the comprehensive support provided by Meraki's Python SDK. 
+
+### Dependencies
+
+<details>
+
+| **Module** | **Documentation** | **Notes** |
+|------------|-------------------|-----------|
+| `meraki`   | [Meraki](https://github.com/meraki/dashboard-api-python/tree/main) | This is the official Dashboard API library (SDK) for Python. |
+| `csv`      | [CSV](https://docs.python.org/3/library/csv.html) | This is a built-in, [Standard Library](https://docs.python.org/3/library/index.html) module. |
+| `getpass`  | [getpass](https://docs.python.org/3/library/getpass.html) | This is a built-in, [Standard Library](https://docs.python.org/3/library/index.html) module. |
+| `tqdm`     | [tqdm](https://github.com/tqdm/tqdm) | This is a mature, stable, efficient, and widely-supported library that provides progress-bar functionality. |
+
+</details>
 
 ## Status
 
 ✅ The current version of the script is stable (1.0), but updates and improvements may be implemented as needed.
-
-## Use Cases
-
-🎯 The script's primary use case is to export BSSID details for e911 services location data, as might be required for large organizations. Here are a few additional use cases:
-
-- **Colleges and Universities**: In a college or university environment, the script can help network administrators identify and manage BSSIDs across multiple campuses, buildings, and access points. This information can be useful for optimizing network performance, monitoring network utilization, and ensuring proper coverage across campus areas.
-
-- **Hospitals and Healthcare Facilities**: For hospitals and healthcare facilities with complex network infrastructure, the script can assist in monitoring and managing BSSIDs. It allows network administrators to track the performance of wireless networks in different areas within the facility, ensuring critical connectivity for medical devices, patient monitoring systems, and staff communication.
-
-- **Enterprise Networks**: In large enterprises with multiple branch offices and locations, the script can provide network administrators with a consolidated view of BSSID details across the organization.  
 
 ## Installation
 
@@ -51,9 +50,11 @@ cd meraki-bssid-dump
 pip3 install --upgrade meraki
 ```
 
-## Configuration
+3. Install the [tqdm library](https://github.com/tqdm/tqdm):
 
-Before running the script, make sure to replace `'Your Meraki API Key'` in the script with your actual Meraki Dashboard API key.
+```bash
+pip3 install --upgrade tqdm
+```
 
 ## Usage
 
@@ -63,19 +64,70 @@ To execute the script, use the following command:
 python3 meraki-bssid-dump.py
 ```
 
-The script will generate a `Meraki-BSSIDs.csv` file in the same directory, containing the fetched BSSID details.
+## Input
 
-## Known Issues
+🗝️ You will be prompted to provide a valid [API Key](https://documentation.meraki.com/Platform_Management/Dashboard_Administration/Operate_and_Maintain/How-Tos/Cisco_Meraki_Dashboard_API#Generate_an_API_Key) for your Meraki account. This is ***required*** in order to access the Meraki Dashboard API. You can read more about generating and managing API keys for the Meraki Dashboard API in the [Cisco Meraki Dashboard Administration documentation](https://documentation.meraki.com/Platform_Management/Dashboard_Administration/Operate_and_Maintain/How-Tos/Cisco_Meraki_Dashboard_API).
 
-⚠️ Running the script on networks with a large number of devices may take some time due to the rate limit imposed by the Meraki API. It is advised to run the script during off-peak hours or schedule it accordingly.
+## Output
+
+💾 The script will generate a `Meraki-BSSIDs.csv` file in the same directory, containing the fetched BSSID details.\
+⚠️ This file will be overwritten with each execution.
+
+<details>
+
+<summary>Sample CSV File Output</summary>
+
+### Sample CSV File Output
+
+| Network ID | Network Name | Device Model | Device Serial  | Device Base MAC   | LAN IP          | Display Name | Location Address   | Longitude | Latitude | Floor Plan ID | Notes     | BSSID              | SSID      | Band  |
+|------------|--------------|--------------|----------------|-------------------|-----------------|--------------|--------------------|-----------|----------|---------------|-----------|--------------------|-----------|-------|
+| A_12345678 | MyNetwork1   | CW1234L      | A12B-CD34-E56F | a1:b2:c3:d4:e5:f6 | 123.456.789.101 | Device1      | 123 A Street NW    | -45.678   |  90.123  | None          |           | 00:11:22:33:44:55  | Network1  | 2.4GHz|
+| A_12345678 | MyNetwork1   | MR99         | GH78-I9J0-1K2L | 1a:2b:3c:4d:5e:6f | 121.314.151.617 | Device1      | 456 D Aveneue SE   | -90.123   | -45.678  | None          |           | 66:77:88:99:AA:BB  | Network1  | 5GHz  |
+| A_12345678 | MyNetwork1   | MR01         | 3M45-O6N8-9Q09 | ab:12:cd:34:ef:56 | 181.920.212.223 | Device2      | 789 13th Blvd      |  45.678   | -90.123  | None          |           | CC:DD:EE:FF:00:11  | Network2  | 2.4GHz|
+
+</details>
+
+## Precautions
+
+⏱️ Running the script for Meraki organizations or networks with a large number of devices will take some time due to the strict rate limits imposed by the Meraki API. It is advised to run the script during off-peak hours or schedule it accordingly. For reference, in testing it has taken 25-30 minutes to process 9,447 BSSIDs on 1,230 devices across 615 networks.
+
+## Limitations
+
+<details>
+
+<summary>Interactive API Key Prompt</summary>
+
+In rare cases, due to limitations in the `getpass` module, the interactive prompt for the Meraki API Key might not work correctly. If you are affected by this, there is commented code you can edit to set your Meraki API Key statically. If you need to apply this override, also comment-out the preceeding while loop that handles the interactive prompt. See the [getpass documentation](https://docs.python.org/3/library/getpass.html) for details on its limitations. 
+
+`getpass` is part of the official [Python Standard Library](https://docs.python.org/3/library/index.html).\
+***It should be extremely unusual for these limitations to impact interactive users.***
+
+</details>
 
 ## Future Improvements
 
-🔮 Future versions of the script may include added features such as a progress display, filtering options, and customized output formats.
+🔮 Future versions of the script might include added features such as:
+- [x] Progress Display
+- [ ] Filtering Options
+- [ ] Customized Output Formats
+- [ ] Interactive Output File Overwrite Hanlding
 
 ## Contributions
 
 🤝 Thanks to Matt Giepert for the bulk of the starting code! Future contributions are welcome! If you have any ideas or improvements, feel free to fork the project and submit a pull request.
+
+## Additional Reading
+
+<details>
+
+<summary>Calculating Cisco Meraki BSSID MAC Addresses</summary>
+
+### Calculating Cisco Meraki BSSID MAC Addresses
+
+The document "[Calculating Cisco Meraki BSSID MAC Addresses](https://documentation.meraki.com/MR/Wi-Fi_Basics_and_Best_Practices/Calculating_Cisco_Meraki_BSSID_MAC_Addresses)" offers a comprehensive guide on how BSSID MAC addresses are computed for Cisco Meraki access points. Each SSID is represented by a unique BSSID, which helps clients identify the associated access point. The BSSID addresses are derived from the wired MAC address, with calculations based on the MAC OUI. Different MAC OUIs for various access point models determine the adjustments for 2.4 GHz and 5 GHz bands. The BSSID values are assigned to each radio and SSID, with up to 15 possible BSSID combinations per radio.
+
+👉 This script does not perform these calculations. It retrieves the BSSIDs from the Meraki Dashboard API.
+</details>
 
 ## License
 
--- meraki-bssid-dump.py
+++ meraki-bssid-dump.py
@@ -1,109 +1,188 @@
 # Import Resources
 import meraki
 import csv
-import re
+import getpass
+from tqdm import tqdm
 
-# Your Meraki API key
-api_key = 'Your Meraki API Key'
+# Function to prompt for the Meraki API Key
+def get_api_key():
+    try:
+        user_api_key = getpass.getpass("Enter your Meraki API Key to continue: ", echo_char='*')
+    except:
+        raise Exception("Unable to prompt for API Key!\n\nYou might need to set your API Key statically in the script file.")
+    else:
+        return user_api_key
 
-#create dashboard object
-dash = meraki.DashboardAPI(api_key, suppress_logging=True)
+# Initialize the api_key variable as an empty string
+api_key = ""
 
-# Initialize output files and write headers (this will erase existing file contents to ensure clean output for each run)
-bssidFileHeaders = ["Network ID", "Network Name", "Device Model", "Device Serial", "Device Base MAC", "LAN IP", "Display Name", "Location Address", "Longitude", "Latitude", "Floor Plan ID", "Notes", "BSSID", "SSID", "Band"]
-with open('Meraki-BSSIDs.csv', 'w', encoding="utf-8", newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(bssidFileHeaders)
+# Request Meraki API Key from the user by looping until a nominally viable entry is provided
+while len(api_key) < 40:
+    try:
+        api_key = get_api_key()
+    except:
+        raise Exception("An error occurred in prompting for the API Key!\n\nStatic configuration might be necessary.")
+    else:
+        if len(api_key) >= 40:
+            print("Thank you for providing your Meraki API Key!")
+        else:
+            print("Your Meraki API Key is required to continue!\nMerkai API Keys should be at least 40 characters long.\n")
 
-skippedFileHeaders = ["Network ID", "Network Name", "Device Model", "Device Serial", "Display Name", "Location Address"]
-with open('Meraki-SkippedDevices.csv', 'w', encoding="utf-8", newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(skippedFileHeaders)
+# Manual / Static Meraki API Key Override
+# If your runtime environment does not support getpass for some reason, you can uncomment this section and set your API Key permanently here.
+# Be aware of the potential risks of storing your API key in a plaintext file like this.
+# api_key = ''
+
+# Create Meraki Dashboard object
+try:
+    dash = meraki.DashboardAPI(api_key, suppress_logging=True)
+except:
+    raise Exception("Unable to define the Meraki Dashboard API connection! Check api_key")
 
 # Initialize counters to track parsing
-orgCount = 0
-netCount = 0
-devCount = 0
-matchCount = 0
-bssidCount = 0
-skipCount = 0
+netCount        = 0
+devCount        = 0
+bssidCount      = 0
+totalNets       = 0
+totalDevices    = 0
+totalOrgs       = 0
 
-#loop through orgs
-for org in dash.organizations.getOrganizations():
-    orgCount = orgCount + 1
-    print(f"Processing organization #{orgCount}: {org['id']}...")
-    #loop through nets
-    for net in dash.organizations.getOrganizationNetworks(org['id']):
-        netCount = netCount + 1
-        netId = str(net['id'])
-        netName = str(net['name'])
-        print(f"  Processing network #{netCount}:")
-        print(f"  ID:   {netId}")
-        print(f"  Name: {netName}")
-        #loop through devices
-        for device in dash.networks.getNetworkDevices(net['id']):
-            devCount = devCount + 1
-            deviceModel = str(device['model'])
-            deviceSerial = str(device['serial']) if device['serial'] is not None else 'Serial Not Found'
-            deviceMacAddress = str(device.get('mac', ''))
-            deviceLanIp = str(device.get('lanIp', ''))
-            deviceName = str(device['name']) if device['name'] is not None else deviceSerial
-            deviceAddress = str(device.get('address', ''))
-            deviceLatitude = str(device.get('lat', ''))
-            deviceLongitude = str(device.get('lng', ''))
-            deviceFloorPlanId = str(device.get('floorPlanId', ''))
-            deviceNotes = str(device.get('notes', ''))
-
-            #check if device is an AP
-            #use regex matching to check start of string
-            if re.match(r"MR",device.get('model', '')) or re.match(r"CW",device.get('model', '')):
-                matchCount = matchCount + 1
-                print(f"    Processing AP {matchCount}:")
-                print(f"      Model:         {deviceModel}")
-                print(f"      Serial:        {deviceSerial}")
-                print(f"      Base MAC:      {deviceMacAddress}")
-                print(f"      LAN IP:        {deviceLanIp}")
-                print(f"      Name:          {deviceName}")
-                print(f"      Location:      {deviceAddress}")
-                print(f"      Longitude:     {deviceLongitude}")
-                print(f"      Latitude:      {deviceLatitude}")
-                print(f"      Floor Plan ID: {deviceFloorPlanId}")
-                print(f"      Notes:         {deviceNotes}")
-                #get BSSID list
-                status = dash.wireless.getDeviceWirelessStatus(device['serial'])
-                #loop through all sets
-                for set in status['basicServiceSets']:
-                    #only report on enabled
-                    if set.get('enabled'):
-                        bssidCount = bssidCount + 1
-                        deviceBssid = str(set['bssid'])
-                        deviceSsid = str(set['ssidName'])
-                        bssidBand = str(set['band'])
-                        print(f"      Processing BSSID: {deviceBssid}")
-                        print(f"        BSSID: {deviceBssid}")
-                        print(f"        SSID:  {deviceSsid}")
-                        print(f"        Band:  {bssidBand}")
-                        list = [netId, netName, deviceModel, deviceSerial, deviceMacAddress, deviceLanIp, deviceName, deviceAddress, deviceLongitude, deviceLatitude, deviceFloorPlanId, deviceNotes, deviceBssid, deviceSsid, bssidBand]
-                        print("      Writing BSSID Info to file:")
-                        for index, item in enumerate(list):
-                            print(f"        {index}: {item}")
-                        with open('Meraki-BSSIDs.csv', 'a', encoding="utf-8", newline='') as csvfile:
-                            writer = csv.writer(csvfile)
-                            writer.writerow(list)
+# Get Meraki Organizations
+print("Fetching Meraki Organizations...")
+try:
+    dashOrgs = dash.organizations.getOrganizations()
+    # Returns a List object
+except:
+    raise Exception("Unable to get Meraki Organizations from the Meraki Dashboard API!")
+else:
+    totalOrgs = len(dashOrgs)
+    # Initialize empty list to hold details for parsed networks
+    allMatchedNetworks = []
+    print(f"  Found {totalOrgs} Meraki Organizations!")
+    # Parse Meraki Organization information
+    if totalOrgs > 0:
+        print("Searching organizations for networks with wireless products...")
+        # Initialize empty dictionary to store Organization names as values for their IDs (as keys)
+        orgDetails = {}
+        for org in dashOrgs:
+            # Normalize keys and values and store in orgDetails dictionary
+            org_id = str(org.get("id", ""))
+            org_name = str(org.get("name", ""))
+            orgDetails[org_id] = org_name
+            try:
+                # Get network details for each organziation
+                orgNetworks = dash.organizations.getOrganizationNetworks(org_id, productTypes=["wireless"], total_pages="all")
+            except:
+                print(f"[!!]An exception occurred when trying to retrieve networks for organization id: {org_id} (\"{org_name}\")")
             else:
-                skipCount = skipCount +1
-                print(f"    Skipping {device['serial']} because model is {device['model']}...")
-                print(f"        Serial: {deviceSerial}")
-                print(f"        Name:   {deviceName}")
-                print(f"        Model:  {deviceModel}")
-                list = [netId, netName, deviceModel, deviceSerial, deviceName, deviceAddress]
-                print("     Writing Skipped Device Info to file:")
-                for index, item in enumerate(list):
-                    print(f"      {index}: {item}")
-                with open('Meraki-SkippedDevices.csv', 'a', encoding="utf-8", newline='') as csvfile:
-                    writer = csv.writer(csvfile)
-                    writer.writerow(list)
-print("Processing completed!")
-print(f"  Evaluated {devCount} devices in {netCount} networks.")
-print(f"  Found {bssidCount} BSSIDs enabled on {matchCount} wireless access points.")
-print(f"  Skipped {skipCount} devices.")
+                # Store data for networks
+                allMatchedNetworks.extend(orgNetworks)
+                orgNetworkCount = len(orgNetworks)
+                print(f"  Organization ID: {org_id} named \"{org_name}\" has {orgNetworkCount} networks with wireless devices")
+        totalNets = len(allMatchedNetworks)
+        print(f"{totalNets} networks with wireless devices were found!")
+    else:
+        print(f"No organizations were found using the provided Meraki Dashboard API key")
+
+if totalNets > 0:
+    print("Collecting wireless device details...")
+    # Initialize empty list to store discovered BSSID data
+    discoveredBSSIDs = []
+    # Loop through networks to collect device information
+    for net in tqdm(allMatchedNetworks, desc="Network", unit="network", miniters=1, position=1, bar_format="{desc} {n_fmt}/{total_fmt} {percentage:.0f}% {bar}Elapsed: {elapsed} ETA: {remaining}"):
+        netCount = netCount + 1
+        netId = str(net.get("id"))
+        netName = str(net.get("name"))
+        orgId = str(net.get("organizationId"))
+        orgName = str(orgDetails.get(orgId, ""))
+        tqdm.write(f"  Organization: {orgId} \"{orgName}\"")
+        tqdm.write(f"    Processing network {netCount}/{totalNets} ID: {netId} Name: {netName}")
+        try:
+            netDevices = dash.organizations.getOrganizationDevices(organizationId=orgId, networkIds=[netId], productTypes="wireless", perPage=5000, total_pages="all")
+        except:
+            print(f"[!!] An excption occurred in processing network ID {netId}")
+        else:
+            devCount = len(netDevices)
+            tqdm.write(f"      {devCount} wireless devices found!")
+            currentNetDevCount = 0
+            for netDevice in tqdm(netDevices, desc="Network Device", unit="device", miniters=1, position=0, leave=False, bar_format="{desc} {n_fmt}/{total_fmt} {percentage:.0f}% {bar}"):
+                currentNetDevCount      = currentNetDevCount + 1
+                totalDevices            = totalDevices + 1
+                tqdm.write(f"        Processing wireless device {currentNetDevCount}/{devCount}:")
+                currentNetDevModel      = str(netDevice['model'])
+                currentNetDevSerial     = str(netDevice['serial'])
+                currentNetDevMacAddress = str(netDevice.get('mac', ""))
+                currentNetDevLanIp      = str(netDevice.get('lanIp', ""))
+                currentNetDevName = str(netDevice['name']) if netDevice['name'] is not None else currentNetDevSerial
+                currentNetDevAddress = str(netDevice.get('address', ''))
+                currentNetDevLatitude = str(netDevice.get('lat', ''))
+                currentNetDevLongitude = str(netDevice.get('lng', ''))
+                currentNetDevFloorPlanId = str(netDevice.get('floorPlanId', ''))
+                currentNetDevNotes = str(netDevice.get('notes', ''))
+                tqdm.write(f"          Model:         {currentNetDevModel}")
+                tqdm.write(f"          Serial:        {currentNetDevSerial}")
+                tqdm.write(f"          Base MAC:      {currentNetDevMacAddress}")
+                tqdm.write(f"          LAN IP:        {currentNetDevLanIp}")
+                tqdm.write(f"          Name:          {currentNetDevName}")
+                tqdm.write(f"          Location:      {currentNetDevAddress}")
+                tqdm.write(f"          Longitude:     {currentNetDevLongitude}")
+                tqdm.write(f"          Latitude:      {currentNetDevLatitude}")
+                tqdm.write(f"          Floor Plan ID: {currentNetDevFloorPlanId}")
+                tqdm.write(f"          Notes:         {currentNetDevNotes}")
+                try:
+                    # get BSSID List
+                    status = dash.wireless.getDeviceWirelessStatus(netDevice['serial'])
+                except:
+                    print(f"[!!] An exception occurred in processing BSSIDs for {currentNetDevSerial}")
+                else:
+                    # Loop through all basic service sets
+                    for set in status['basicServiceSets']:
+                        # Only report on enabled BSSIDs
+                        if set.get('enabled'):
+                            bssidCount = bssidCount +1
+                            deviceBssid = str(set['bssid'])
+                            deviceSsid = str(set['ssidName'])
+                            bssidBand = str(set['band'])
+                            tqdm.write(f"      Processing BSSID: {deviceBssid}")
+                            tqdm.write(f"        BSSID: {deviceBssid}")
+                            tqdm.write(f"        SSID:  {deviceSsid}")
+                            tqdm.write(f"        Band:  {bssidBand}")
+                            # Construct dictionary for current output
+                            currentBSSIDoutput = {
+                                "netId"             :   netId,
+                                "netName"           :   netName,
+                                "deviceModel"       :   currentNetDevModel,
+                                "deviceSerial"      :   currentNetDevSerial,
+                                "deviceMacAddress"  :   currentNetDevMacAddress,
+                                "deviceLanIp"       :   currentNetDevLanIp,
+                                "deviceName"        :   currentNetDevName,
+                                "deviceAddress"     :   currentNetDevAddress,
+                                "deviceLongitude"   :   currentNetDevLongitude,
+                                "deviceLatitude"    :   currentNetDevLatitude,
+                                "deviceFloorPlanId" :   currentNetDevFloorPlanId,
+                                "deviceNotes"       :   currentNetDevNotes,
+                                "deviceBssid"       :   deviceBssid,
+                                "deviceSsid"        :   deviceSsid,
+                                "bssidBand"         :   bssidBand
+                                }
+                            # Append current dictionary to output list
+                            discoveredBSSIDs.append(currentBSSIDoutput)
+    print("Processing completed!")
+    print(f"  Evaluated {totalDevices} devices in {netCount} networks.")
+    print(f"  Found {bssidCount} BSSIDs enabled on {totalDevices} wireless access points.")
+    # Define the field names (keys of the dictionaries) that will become the header row
+    fieldnames = ["netId", "netName", "deviceModel", "deviceSerial", "deviceMacAddress", "deviceLanIp", "deviceName", "deviceAddress", "deviceLongitude", "deviceLatitude", "deviceFloorPlanId", "deviceNotes", "deviceBssid", "deviceSsid", "bssidBand"]
+    print(f"Writing {len(discoveredBSSIDs)} BSSIDs to Meraki-BSSIDs.csv")
+    try:
+        with open('Meraki-BSSIDs.csv', 'w', encoding="utf-8", newline='') as csvfile:
+            # Create a DictWriter object
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            # Write header row to CSV
+            writer.writeheader()
+            # Write data to CSV
+            writer.writerows(discoveredBSSIDs)
+        print("Finished writing to Meraki-BSSIDs.csv!")
+    except IOError as e:
+        print(f"I/O error: {e}")
+else:
+    print("No wireless devices to evaluate!")
